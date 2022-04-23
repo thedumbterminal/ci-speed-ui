7d2fc1222081211942172e6f92d8206980bd9b26
The Dumb Terminal <github@thedumbterminal.co.uk>
2022-04-24 00:49:03 +0100

    started converting to data grid

--- src/pages/test_runs/index.tsx
+++ src/pages/test_runs/index.tsx
@@ -1,64 +1,88 @@
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
+import { DataGrid, GridColDef, GridValueFormatterParams, GridRenderCellParams } from '@mui/x-data-grid'
 import { format } from 'date-fns'
 import Link from 'next/link'
 import TestRun from '../../shared/TestRun'
 import api from '../../api'
 import { NextPage } from 'next'
 
-interface TestRunsProps {
-  testRuns: TestRun[]
+interface TestRunRow {
+  id: number,
+  created: string
 }
 
-const formatDate = (isoString: string): string => {
-  const date = new Date(isoString)
+interface TestRunsProps {
+  testRuns: TestRunRow[]
+}
+
+const formatDate = (params: GridValueFormatterParams<string>): string => {
+  const date = new Date(params.value)
   return format(date, 'MM/dd/yyyy kk:mm:ss')
 }
 
-const renderTestRun = (testRun: TestRun) => {
-  const testRunLink = `/test_runs/${testRun.id}`
+const formatLink = (params: GridValueFormatterParams<string>): string => {
+  return `/test_runs/${params.value}`
+}
+
+const transformRows = (testRuns: TestRun[]): TestRunRow[] => {
+  return testRuns.map((testRun: TestRun) => {
+    return {
+      id: testRun.id,
+      created: testRun.created_at
+    }
+  })
+}
+
+const renderLinkCell = (params: GridRenderCellParams<string>) => {
+  const formatted = params.formattedValue as string
   return (
-    <TableRow
-      key={testRun.id}
-      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-    >
-      <TableCell>{formatDate(testRun.created_at)}</TableCell>
-      <TableCell>
-        <Link href={testRunLink}>View test suites</Link>
-      </TableCell>
-    </TableRow>
+    <Link href={formatted}>Test suites</Link> 
   )
 }
+
+const columns: GridColDef[] = [
+  {
+    field: 'created',
+    headerName: 'Created',
+    width: 160,
+    valueFormatter: formatDate
+  },
+  {
+    field: 'id',
+    headerName: 'View',
+    width: 160,
+    valueFormatter: formatLink,
+    renderCell: renderLinkCell
+  }
+]
 
 const TestRuns: NextPage<TestRunsProps> = ({ testRuns }: TestRunsProps) => {
   return (
     <>
       <h1>Test Runs</h1>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Created</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {testRuns.map((testRun: TestRun) => renderTestRun(testRun))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <DataGrid
+        rows={testRuns}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[5]}
+        autoHeight={true}
+        disableColumnMenu={true}
+        disableSelectionOnClick={true}
+        sx={{
+          boxShadow: 2,
+          border: 2,
+          borderColor: 'primary.light',
+          '& .MuiDataGrid-cell--editable': {
+          }
+        }}
+      />
     </>
   )
 }
 
 TestRuns.getInitialProps = async (): Promise<TestRunsProps> => {
   const data = await api.get('/test_runs/')
-  return { testRuns: data }
+  const rows = transformRows(data)
+  return { testRuns: rows }
 }
 
 export default TestRuns
