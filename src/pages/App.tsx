448d0fdaa0c26bf0e0544012b49a7201b0f33f6b
The Dumb Terminal <github@thedumbterminal.co.uk>
2022-05-01 22:16:18 +0100

    add error boundary

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,16 +3,18 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../theme';
 import Layout from '../components/Layout'
-
+import ErrorBoundary from '../components/ErrorBoundary'
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      <Layout>
-        <Outlet />
-      </Layout>
+      <ErrorBoundary>
+        <Layout>
+          <Outlet />
+        </Layout>
+      </ErrorBoundary>
     </ThemeProvider>
-  );
+  )
 }
