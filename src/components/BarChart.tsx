74b7ab5243106c74910b44151b848c4d398467cc
The Dumb Terminal <github@thedumbterminal.co.uk>
2022-05-30 19:45:38 +0100

    a working chart

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -8,34 +8,37 @@ import {
   BarGroup
 } from '@visx/xychart';
 
+import {RenderTooltipParams} from '@visx/xychart/lib/components/Tooltip'
 export type BarsProps = {
   height: number;
 };
 
 type Datum = { x: string; y: number };
 
-const Chart = ({ height }: BarsProps) => {
+const accessors = {
+  xAccessor: (d: Datum) => d.x,
+  yAccessor: (d: Datum) => d.y,
+};
 
-  const data1 = [
-    { x: '2020-01-01', y: 50 },
-    { x: '2020-01-02', y: 10 },
-    { x: '2020-01-03', y: 20 },
-  ];
-    
-  const accessors = {
-    xAccessor: (d: Datum) => d.x,
-    yAccessor: (d: Datum) => d.y,
-  };
-
-  const renderTooltip = ({ tooltipData, colorScale }) => (
+const renderTooltip = ({ tooltipData, colorScale }: RenderTooltipParams<Datum>) => {
+  const datum = tooltipData?.nearestDatum?.datum
+  return (
     <>
-      <div style={{ color: colorScale(dataKey) }}>{dataKey}</div>
+      <div style={{ color: colorScale && colorScale(tooltipData?.nearestDatum?.key || '') }}>{tooltipData?.nearestDatum?.key}</div>
       <br />
-      {xAccessor(tooltipData.datumByKey[dataKey].datum)}:{" "}
-      {yAccessor(tooltipData.datumByKey[dataKey].datum).toFixed(2)}
+      {datum && accessors.xAccessor(datum)}:{" "}
+      {datum && accessors.yAccessor(datum).toFixed(2)}
     </>
   )
+}
 
+const data1 = [
+  { x: '2020-01-01', y: 50 },
+  { x: '2020-01-02', y: 10 },
+  { x: '2020-01-03', y: 20 },
+];
+
+const Chart = ({ height }: BarsProps) => {
   return (
     <XYChart height={height} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
       <AnimatedAxis orientation="bottom" />
