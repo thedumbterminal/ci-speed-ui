5791feca7d2555d7180450eabdf1e24ffede3310
thedumbterminal <github@thedumbterminal.co.uk>
2022-06-13 23:54:13 +0100

    now showing test errors

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,29 @@
 import { Outlet } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
+import GlobalStyles from '@mui/material/GlobalStyles'
 import theme from '../theme'
 import Layout from '../components/Layout'
 import ErrorBoundary from '../components/ErrorBoundary'
+
+const inputGlobalStyles = (
+  <GlobalStyles
+    styles={{
+      'pre.failure': {
+        color: 'white',
+        'background-color': 'black',
+        padding: '10px',
+      },
+    }}
+  />
+)
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
+      {inputGlobalStyles}
       <ErrorBoundary>
         <Layout>
           <Outlet />
--- src/pages/TestSuite.tsx
+++ src/pages/TestSuite.tsx
@@ -1,29 +1,53 @@
-import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import {
+  DataGrid,
+  GridColDef,
+  GridValueFormatterParams,
+  GridRenderCellParams,
+} from '@mui/x-data-grid'
 import { api } from '../lib/api'
 import useSWR from 'swr'
-import { useSearchParams } from 'react-router-dom'
 import Typography from '@mui/material/Typography'
+import { Link, useSearchParams } from 'react-router-dom'
 
 interface TestCaseRow {
   id: number
   name: string
   duration: number
+  status: string
+  failure_id: number | undefined
 }
 
 interface TestCase {
   name: string
   time: number
   id: number
+  test_failures: Array<number>
 }
 
-const transformRows = (testRuns: TestCase[]): TestCaseRow[] => {
-  return testRuns.map((item: TestCase) => {
-    return {
-      id: item.id,
-      name: item.name,
-      duration: item.time,
-    }
-  })
+const _formatLink = (value: string): string => {
+  return `/test_failure/?id=${value}`
+}
+
+const _renderLinkCell = (params: GridRenderCellParams<string>) => {
+  if (!params.formattedValue) return
+  const formattedLink = _formatLink(params.formattedValue)
+  return <Link to={formattedLink}>View error</Link>
+}
+
+const _transformRow = (item: TestCase) => {
+  const status = item.test_failures.length ? 'Failure' : 'Success'
+  const failure_id = item.test_failures[0]
+  return {
+    id: item.id,
+    name: item.name,
+    duration: item.time,
+    status,
+    failure_id,
+  }
+}
+
+const _transformRows = (testCases: TestCase[]): TestCaseRow[] => {
+  return testCases.map(_transformRow)
 }
 
 const columns: GridColDef[] = [
@@ -37,6 +61,17 @@ const columns: GridColDef[] = [
     headerName: 'Duration',
     width: 180,
   },
+  {
+    field: 'status',
+    headerName: 'Status',
+    width: 180,
+  },
+  {
+    field: 'failure_id',
+    headerName: 'View',
+    width: 160,
+    renderCell: _renderLinkCell,
+  },
 ]
 
 const _getPageData = (id: string) => {
@@ -45,7 +80,7 @@ const _getPageData = (id: string) => {
     api.get
   )
   const { data: testCases, error: caseError } = useSWR(
-    () => ['/test_cases/', { test_suite_id: testSuite.id }],
+    () => ['/test_cases/', { test_suite_id: id }],
     api.get
   )
   return {
@@ -63,7 +98,7 @@ const TestSuite = () => {
   const { data, error, isLoading } = _getPageData(testSuiteId)
   if (error) throw error
   if (data?.testCases) {
-    testCases = transformRows(data.testCases)
+    testCases = _transformRows(data.testCases)
   }
 
   return (
